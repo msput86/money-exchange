24ae7d2c40c3318486dbb12b253967507f6edc05
nozrevatykh <Michael.Putov@artezio.com>
2019-02-26 17:06:14 +0300

    relolved

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency<=0){
+        return {};
+    }
+    if (currency>10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    var result = {};
+    config.forEach(item=>{
+       var q= parseInt(currency/item.value);
+       if (q>0){
+           result[item.label] = q;
+           currency = currency - item.value*q;
+       }
+    });
+    return result;
 }
+config = [
+    {label:"H", value: 50},
+    {label:"Q", value: 25},
+    {label:"D", value: 10},
+    {label:"N", value: 5},
+    {label:"P", value: 1}
+]
